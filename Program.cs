35e74a7655db19ed38b955f225dae1b6eee722c4
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-01-05 01:49:07 -0300

    Finalizando conceitos de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:\n");
+Smartphone nokia = new Nokia(numero:"123456", modelo: "tijolão", imei: "11111111", memoria:512);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Clash Royale");
+
+Console.WriteLine("\nIphone:\n");
+Smartphone iphone = new Iphone(numero:"654321", modelo: "Iphone 14", imei: "22222222", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
